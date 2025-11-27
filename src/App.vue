9be14bec50018cb9aa589d061028ceb717c66caf
huynhkhoinguyen251 <emailken098@gmail.com>
2025-11-27 19:26:55 +0700

    update1

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,59 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+    <div class="ui inverted segment navbar">
+      <div class="ui center aligned container">
+        <div class="ui large secondary inverted pointing menu compact">
+          <router-link to="/words" exact class="item">
+            <i class="comment outline icon"></i> Words
+          </router-link>
+          <router-link to="/words/new" class="item">
+            <i class="plus circle icon"></i> New
+          </router-link>
+          <router-link to="/test" class="item">
+            <i class="graduation cap icon"></i> Test
+          </router-link>
+        </div>
+      </div>
+    </div>
+
+    <div class="ui text container">
+      <div class="ui one column grid">
+        <div class="column">
+          <router-view />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
+<script>
+export default {
+  name: 'app'
+};
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+  #app > div.navbar {
+    margin-bottom: 1.5em;
+  }
+  .myFlash {
+    width: 250px;
+    margin: 10px;
+    position: absolute;
+    top: 50;
+    right: 0;
+  }
+  input {
+    width: 300px;
+  }
+  div.label {
+    width: 120px;
+  }
+  div.input {
+    margin-bottom: 10px;
+  }
+  button.ui.button {
+    margin-top: 15px;
+    display: block;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,9 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   render: function (h) { return h(App) }
 }).$mount('#app')
+  
